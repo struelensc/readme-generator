342cf8859dd3ee99e64c82de8431bcbeb3871616
Courtney Struelens <courtneystruelens@hotmail.com>
2022-04-23 19:09:46 -0700

    Separated license text into it's own file and fixed table of contents bugs

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,3 +1,5 @@
+const licenseReturn = require("./stringConstants");
+
 // Returns a badge for chosen license or returns null if no license was chosen
 function renderLicenseBadge(license) {
   if (license === "MIT License") {
@@ -26,54 +28,22 @@ function renderLicenseLink(data, license) {
 
 // Returns text for LICENSE file or returns null if no license was chosen
 function renderLicenseText(license, names) {
+  let copyright;
+  let licenseText = licenseReturn(license);
+
   if (license === "MIT License") {
-    return `Copyright ${new Date().getFullYear()} ${names}
-
-    Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
-    
-    The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-    
-    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.`;
+    copyright = `Copyright ${new Date().getFullYear()} ${names}`;
   } else if (license === "GNU General Public License (GPL) 2.0") {
-    return `Copyright (C) ${new Date().getFullYear()} ${names}
-
-    This program is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation; either version 2 of the License, or (at your option) any later version.
-    
-    This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
-    
-    You should have received a copy of the GNU General Public License along with this program; if not, write to the Free Software Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA`;
+    copyright = `Copyright (C) ${new Date().getFullYear()} ${names}`;
   } else if (license === "Apache License 2.0") {
-    return `Copyright ${new Date().getFullYear()} ${names}
-
-    Licensed under the Apache License, Version 2.0 (the "License");
-    you may not use this file except in compliance with the License.
-    You may obtain a copy of the License at
- 
-      http://www.apache.org/licenses/LICENSE-2.0
- 
-    Unless required by applicable law or agreed to in writing, software
-    distributed under the License is distributed on an "AS IS" BASIS,
-    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-    See the License for the specific language governing permissions and
-    limitations under the License.`;
+    copyright = `Copyright ${new Date().getFullYear()} ${names}`;
   } else if (license === "GNU General Public License (GPL) 3.0") {
-    return `Copyright (C) ${new Date().getFullYear()} ${names}
-
-    This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
-
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with this program.  If not, see <http://www.gnu.org/licenses/>.`;
+    copyright = `Copyright (C) ${new Date().getFullYear()} ${names}`;
   } else {
     return;
   }
+
+  return `${copyright}\n\n${licenseText}`;
 }
 
 // Renders table of contents
@@ -120,7 +90,10 @@ function generateMarkdown(data) {
       header: "## Table of Contents",
       info: data,
       textContent: () => {
-        return renderTOC(readmeSections);
+        let text = renderTOC(readmeSections);
+        if (text) {
+          return text;
+        }
       },
     },
     {
---
+++ utils/stringConstants.js
@@ -0,0 +1,69 @@
+const MIT = `Permission is hereby granted, free of charge, to any person 
+obtaining a copy of this software and associated documentation files (the "Software"), 
+to deal in the Software without restriction, including without limitation the rights to use, 
+copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, 
+and to permit persons to whom the Software is furnished to do so, 
+subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included 
+in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, 
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR 
+COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF 
+CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR 
+THE USE OR OTHER DEALINGS IN THE SOFTWARE.`;
+
+const apache = `Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+  http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.`;
+
+const GPL3 = `This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.`;
+
+const GPL2 = `This program is free software; you can redistribute it 
+and/or modify it under the terms of the GNU General Public License as 
+published by the Free Software Foundation; either version 2 of the License, 
+or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; 
+without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
+See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along with this program; 
+if not, write to the Free Software Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA`;
+
+function licenseReturn(request) {
+  if (request === "MIT License") {
+    return MIT;
+  } else if (request === "GNU General Public License (GPL) 2.0") {
+    return GPL2;
+  } else if (request === "Apache License 2.0") {
+    return apache;
+  } else if (request === "GNU General Public License (GPL) 3.0") {
+    return GPL3;
+  } else {
+    return;
+  }
+}
+
+module.exports = licenseReturn;
